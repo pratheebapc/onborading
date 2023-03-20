77e7fbab003fb9c93fbc86ed64466fb11f9ac0dd
shadowshan125 <ajaykrishnan114cs@gmail.com>
2023-03-20 11:53:40 +0530

    Firstcommit

---
+++ src/test/java/com/zaga/employeemangement/EmployeeMangementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zaga.employeemangement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeMangementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
